4211a99fff6ff970ef8991c66ed6b558dbbb047b
sagomezmartinez <sagomezmartinez@USQROSAGOMEZMA1.us.deloitte.com>
2023-09-14 12:12:08 -0500

    Getting Started with Queue - Second Commit

--- src/main/java/com/demo/publisher/RealTimeExample.java
+++ src/main/java/com/demo/publisher/RealTimeExample.java
@@ -4,16 +4,18 @@ import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
 import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.json.JSONObject;
 
-public class Publisher {
-
+public class RealTimeExample {
+	
 	public static void main(String[] args) {
 		
 		ConnectionFactory factory = new ActiveMQConnectionFactory("admin","admin",
@@ -21,28 +23,26 @@ public class Publisher {
 		
 		try {
 			Connection connection = factory.createConnection();
+			
 			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 			Destination destination = session.createQueue("demo");
 			
-			String[] messages = {"First Message", "Second Message", "Third Message",
-					"Fourth Message"};
+			JSONObject json = new JSONObject();
+			json.put("from_date", "01-Jan-2019");
+			json.put("to_date", "31-Dec-2019");
+			json.put("email", "xyz@gmail.com");
+			json.put("query", "select * from data");
+			
+			TextMessage textMessage = session.createTextMessage(json.toString());
 			
 			MessageProducer producer = session.createProducer(destination);
-			
-			for(String message : messages) {
-				TextMessage textMessage = session.createTextMessage(message);
-				producer.send(textMessage);
-			}
-			
-			System.out.println("Message Published");
+			producer.send(textMessage);
 			
 			session.close();
 			connection.close();
-			
 		} catch(JMSException e) {
 			e.printStackTrace();
 		}
 		
 	}
-
 }
